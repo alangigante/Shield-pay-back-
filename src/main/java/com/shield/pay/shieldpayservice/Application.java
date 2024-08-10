24305cc1ef1fdb58dbf6bca82625f05cf106f93f
alangigante <alandepaulajr@gmail.com>
2024-08-10 12:21:50 -0300

    config db on application.yaml

--- src/main/java/com/shield/pay/shieldpayservice/Application.java
+++ src/main/java/com/shield/pay/shieldpayservice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShieldPayServiceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShieldPayServiceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
