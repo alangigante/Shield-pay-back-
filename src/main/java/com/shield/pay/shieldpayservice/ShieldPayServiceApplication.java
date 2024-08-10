9d6c2cdbf4984e632f1a69cfa849ce77fe1f1596
alangigante <alandepaulajr@gmail.com>
2024-08-10 11:50:25 -0300

    first commit

---
+++ src/main/java/com/shield/pay/shieldpayservice/ShieldPayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shield.pay.shieldpayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldPayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShieldPayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shield/pay/shieldpayservice/ShieldPayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shield.pay.shieldpayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldPayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
